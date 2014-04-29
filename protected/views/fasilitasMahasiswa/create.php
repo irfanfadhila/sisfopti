f485dfa5ba024ae79645f2de33b8b2a174f8bca8
pramkimpam <pramanayuda772@gmail.com>
2014-04-30 05:24:20 +0700

    ubah fasilitas mahasiswa

--- protected/views/fasilitasMahasiswa/create.php
+++ protected/views/fasilitasMahasiswa/create.php
@@ -20,4 +20,4 @@ $this->menu=array(
 
 <h1>Request Fasilitas Mahasiswa</h1>
 
-<?php $this->renderPartial('_form', array('model'=>$model, 'model2'=>$model2)); ?>
+<?php $this->renderPartial('_form', array('model'=>$model, 'serreq'=>$serreq)); ?>
