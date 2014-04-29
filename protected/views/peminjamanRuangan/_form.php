a5705a6230b8a5cfe94cb15b1bd31efbf49c0c4c
pramkimpam <pramanayuda772@gmail.com>
2014-04-30 05:25:25 +0700

    ubah peminjaman ruangan

--- protected/views/peminjamanRuangan/_form.php
+++ protected/views/peminjamanRuangan/_form.php
@@ -26,53 +26,107 @@
 		<?php echo $form->error($model,'id_service_request'); ?>
 	</div>
 -->
-
+<table>
+	<tr>
 	<div class="row">
-		<?php echo $form->labelEx($model2,'tgl_mulai'); ?>
-		<?php echo $form->textField($model2,'tgl_mulai',array('size'=>30,'maxlength'=>30)); ?>
-		<?php echo $form->error($model2,'tgl_mulai'); ?>
+		 <td><?php echo $form->labelEx($serreq,'tgl_mulai'); ?></td>
+	          <td>      <?php 
+	                $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+	                        'name'=>'ServiceRequest[tgl_mulai]',
+	                        'id'=>'ServiceRequest_tgl_mulai',
+	                        'value'=>$serreq->tgl_mulai,
+	                        // additional javascript options for the date picker plugin
+	                        'options'=>array(
+	                                'dateFormat'=>'yy-mm-dd',
+	                                'showAnim'=>'fold',
+	                        ),
+	                        'htmlOptions'=>array(
+	                                'style'=>'height:20px;'
+	                        ),
+	                ));
+	                ?>              </td>
+		<td><?php echo $form->error($serreq,'tgl_mulai'); ?></td>
 	</div>
-
+	</tr>
+	<tr>
 	<div class="row">
-		<?php echo $form->labelEx($model2,'tgl_selesai'); ?>
-		<?php echo $form->textField($model2,'tgl_selesai',array('size'=>30,'maxlength'=>30)); ?>
-		<?php echo $form->error($model2,'tgl_selesai'); ?>
+		 <td><?php echo $form->labelEx($serreq,'tgl_selesai'); ?></td>
+	             <td>   <?php 
+	                $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+	                        'name'=>'ServiceRequest[tgl_selesai]',
+	                        'id'=>'tgl_selesai',
+	                        'value'=>$serreq->tgl_mulai,
+	                        // additional javascript options for the date picker plugin
+	                        'options'=>array(
+	                                'dateFormat'=>'yy-mm-dd',
+	                                'showAnim'=>'fold',
+	                        ),
+	                        'htmlOptions'=>array(
+	                                'style'=>'height:20px;'
+	                        ),
+	                ));
+	                ?> </td>             
+		<td><?php echo $form->error($serreq,'tgl_selesai'); ?></td>
 	</div>
-
+	</tr>
+	<tr>
 	<div class="row">
-		<?php echo $form->labelEx($model2,'keperluan'); ?>
-		<?php echo $form->textField($model2,'keperluan',array('size'=>30,'maxlength'=>30)); ?>
-		<?php echo $form->error($model2,'keperluan'); ?>
+		<td><?php echo $form->labelEx($serreq,'keperluan'); ?></td>
+		<td><?php echo $form->textField($serreq,'keperluan',array('size'=>30,'maxlength'=>30)); ?></td>
+		<td><?php echo $form->error($serreq,'keperluan'); ?></td>
 	</div>
-
+	</tr>
+	<tr>
 	<div class="row">
-		<?php echo $form->labelEx($model,'jumlah_peserta'); ?>
-		<?php echo $form->textField($model,'jumlah_peserta'); ?>
-		<?php echo $form->error($model,'jumlah_peserta'); ?>
+		<td><?php echo $form->labelEx($model,'jumlah_peserta'); ?></td>
+		<td><?php echo $form->textField($model,'jumlah_peserta'); ?></td>
+		<td><?php echo $form->error($model,'jumlah_peserta'); ?></td>
 	</div>
-
+	</tr>
+	<tr>
 	<div class="row">
-		<?php echo $form->labelEx($model,'nama_ruang'); ?>
-		<?php echo $form->textField($model,'nama_ruang',array('size'=>30,'maxlength'=>30)); ?>
-		<?php echo $form->error($model,'nama_ruang'); ?>
+		<td><?php echo $form->labelEx($model,'nama_ruang'); ?></td>
+		<td><?php echo $form->textField($model,'nama_ruang',array('size'=>30,'maxlength'=>30)); ?></td>
+		<td><?php echo $form->error($model,'nama_ruang'); ?></td>
 	</div>
-
+	</tr>
+	<tr>
 	<div class="row">
-		<?php echo $form->labelEx($model,'waktu_pinjam'); ?>
-		<?php echo $form->textField($model,'waktu_pinjam',array('size'=>20,'maxlength'=>20)); ?>
-		<?php echo $form->error($model,'waktu_pinjam'); ?>
+		<td><?php echo $form->labelEx($model,'waktu_pinjam'); ?></td>
+		<td><?php echo $form->textField($model,'waktu_pinjam',array('size'=>20,'maxlength'=>20)); ?></td>
+		<td><?php echo $form->error($model,'waktu_pinjam'); ?></td>
 	</div>
-
+	</tr>
+	<tr>
 	<div class="row">
-		<?php echo $form->labelEx($model,'keperluan_tambahan'); ?>
-		<?php echo $form->textArea($model,'keperluan_tambahan',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'keperluan_tambahan'); ?>
+		<td><?php echo $form->labelEx($model,'keperluan_tambahan'); ?></td>
+		<td><?php echo $form->textArea($model,'keperluan_tambahan',array('rows'=>6, 'cols'=>50)); ?></td>
+		<td><?php echo $form->error($model,'keperluan_tambahan'); ?></td>
 	</div>
-
+	</tr>
+<!--
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
+-->
+	<tr>
+		<div class="row">
+			<td><?php echo $form->labelEx($serreq,'reviewer1'); ?></td>
+			<td><?php echo CHtml::activeDropDownList($serreq, 'id_reviewer1', $serreq->getReviewer(),
+			array('prompt' => '(Select reviewer)')
 
+		); ?></td>
+		</div>
+	</tr>
+	<tr>
+	<div class="row buttons">
+		<td><?php echo CHtml::submitButton('Submit', array('name' => 'button1')); ?>
+		<? echo '&nbsp;&nbsp;&nbsp;'; ?>
+		<?php echo CHtml::submitButton('Save', array('name' => 'button2')); ?></td> 
+
+	</div>
+	</tr>
+</table>
 <?php $this->endWidget(); ?>
 
 </div><!-- form -->
